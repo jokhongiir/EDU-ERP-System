d0fefe9f951d75d1c0b356452de0c8868cbc8aed
raxmatjonoww <raxmatjonovjahongir5@gmail.com>
2026-09-19 12:40:13 +0500

    Update

--- src/pages/Payments/Payments.jsx
+++ src/pages/Payments/Payments.jsx
@@ -98,8 +98,8 @@ export default function Payments({ activeBranch }) {
     }
     .center { text-align: center; }
     .logo {
-      width: 70px;
-      height: 70px;
+      width: 100px;
+      height: 100px;
       margin: 0 auto 3px;
       display: block;
     }
